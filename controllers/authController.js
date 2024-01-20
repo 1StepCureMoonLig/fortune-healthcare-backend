60b926c69297e867cfcf04ea0239db36128b3c06
samprit.c <samprit.c@media.net>
2024-01-21 02:35:56 +0530

    credentials fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,15 @@ const jwt = require('jsonwebtoken')
 const debug = require('debug')('server:app')
 const maxAge = 365 * 24 * 60 * 60 * 1000; //365 days
 
-const createToken = (id) =>{
+const createToken = (id) => {
     const secretKey = "Mnet2024"
-    const options = { expiresIn: '365d'}
-    return jwt.sign({id}, secretKey, options)
+    const options = { expiresIn: '365d' }
+    return jwt.sign({ id }, secretKey, options)
 }
 
 const signup = async (req, res) => {
     const { name, email, password } = req.body;
-    const response={}
+    const response = {}
     try {
         const newUser = await User.create({
             name, email, password
@@ -23,38 +23,35 @@ const signup = async (req, res) => {
         // }
         // 
         // res.cookie("ftune", token,options)
-        response.success="User created successfully"
+        response.success = "User created successfully"
         res.status(201).json(newUser._id);
     } catch (err) {
         debug(err)
         if (err.code === 11000) {
-            response.error="Email is already registered"
-            res.status(400).json(response); 
+            response.error = "Email is already registered"
+            res.status(400).json(response);
             return;
         }
-        response.error=err
+        response.error = err
         res.status(500).json(response);
     }
 }
 
 
-const login = async (req, res) =>{
-    const {email,password} = req.body
-    const response={}
-    try{
-        const user = await User.login(email,password)
+const login = async (req, res) => {
+    const { email, password } = req.body
+    const response = {}
+    try {
+        const user = await User.login(email, password)
         debug(user)
         const token = createToken(user._id);
-        const options = {
-            httpOnly:true,
-            maxAge: maxAge
-        }
-        res.cookie("ftune", token,options)
-        response.user = {user: user._id}
-        res.status(200).json({user: user._id})
-    }catch (err){
+        const options = { secure: true, sameSite: 'None' }
+        res.cookie("ftune", token, options)
+        response.user = { user: user._id }
+        res.status(200).json({ user: user._id })
+    } catch (err) {
         response.Error = err.Error;
         res.status(400).json(response);
     }
 }
-module.exports = {signup, login}
+module.exports = { signup, login }
