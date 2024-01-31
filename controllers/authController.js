3a7c4ce912d0493c5f143a8046273f213431f5bc
deval.g <deval.g@media.net>
2024-01-31 18:06:17 +0530

    Jay Shree Ram

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,7 +45,7 @@ const login = async (req, res) => {
         const user = await User.login(email, password)
         debug(user)
         const token = createToken(user._id);
-        const options = { secure: true, domain:'1stepcure.com', sameSite: 'None' };
+        const options = { secure: true, domain: '1stepcure.com', sameSite: 'None', httpOnly: true };
         res.cookie("ftune", token, options)
         response.user = { user: user._id }
         res.status(200).json({ user: user._id })
