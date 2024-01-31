14dafa04e542c026527b7152dafd01f22efc9a64
samprit.c <samprit.c@media.net>
2024-01-31 11:04:56 +0530

    fix cookie issue

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const initRoutes = require('./routes')
 const app = express()
 const options = {
 	credentials: true,
-	origin: ['https://1stepcure.com', 'https://main--melodic-macaron-19a5fa.netlify.app', 'http://localhost:3000']
+	origin: ['https://1stepcure.com', 'https://main--melodic-macaron-19a5fa.netlify.app', 'http://localhost:3000', 'https://main--chic-sable-f59f7b.netlify.app/']
 }
 
 app.use(cors(options))
