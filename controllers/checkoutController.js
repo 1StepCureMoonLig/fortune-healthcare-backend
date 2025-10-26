00dfb18915d9a93706669ba1a0303d2ea6d7c55c
deval-gajjar <dmg9351@gmail.com>
2025-10-26 13:53:38 +0530

    sendgrid

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,131 +1,148 @@
 const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const debug = require("debug")("server:app");
 
 async function sendEmail(to, subject, html) {
-  debug("inside mail");
+    debug("inside mail");
     console.log("insidemaillll");
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.gmailUser,
-            pass: process.env.gPass,
-        },
-    });
-    console.log("transporter")
-  const mailOptions = {
-    from: "1stepcure@gmail.com", // Replace with your email
-    to: to,
-    subject: subject,
-    html: html,
-  };
+    // const transporter = nodemailer.createTransport({
+    //     host: "smtp.gmail.com",
+    //     port: 465,
+    //     secure: true, // Use SSL
+    //     auth: {
+    //         user: process.env.gmailUser,
+    //         pass: process.env.gPass,
+    //     },
+    // });
+    // console.log("transporter")
+    // const mailOptions = {
+    //     from: "1stepcure@gmail.com", // Replace with your email
+    //     to: to,
+    //     subject: subject,
+    //     html: html,
+    // };
+    // try {
+    //     await transporter.verify();
+    //     console.log("SMTP connection verified");
+    // } catch (error) {
+    //     console.error("SMTP verification failed:", error.message);
+    //     throw error;
+    // }
+    // await transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         console.log("errorrrrrrrrrrrrrrrrrr", error);
+    //     } else {
+    //         console.log("Email sent: " + info.response);
+    //     }
+    // });
+    const msg = {
+        to: to,
+        from: '1stepcure@gmail.com', // Verify this in SendGrid
+        subject: subject,
+        html: html,
+    };
+
     try {
-        await transporter.verify();
-        console.log("SMTP connection verified");
+        await sgMail.send(msg);
+        console.log('Email sent successfully');
     } catch (error) {
-        console.error("SMTP verification failed:", error.message);
+        console.error('Email error:', error.response?.body || error.message);
         throw error;
     }
-  await transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.log("errorrrrrrrrrrrrrrrrrr", error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
 }
 
 module.exports = {
-  mailCheckoutDetails: async (req, res) => {
-      const {
-          firstName,
-          lastName,
-          email,
-          phoneNumber,
-          address,
-          creditCardDetails,
-          billingAddress,
-          userOrder,
-          Total,
-          note,
-      } = req.body;
-      if (Total <= 25) {
-          res.status(400).json("invalid userdata");
-          return;
-      }
-      let htmlBody = `<p>Dear ${firstName} ${lastName},</p>`;
-      htmlBody += `<p>Contact Number: ${phoneNumber}</p>`;
-      htmlBody += `<p>Your order details:</p>`;
-      htmlBody += `<table border="1" cellpadding="5"><thead><tr><th>Product</th><th>Size</th><th>Quantity</th><th>Price</th></tr></thead><tbody>`;
+    mailCheckoutDetails: async (req, res) => {
+        const {
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            address,
+            creditCardDetails,
+            billingAddress,
+            userOrder,
+            Total,
+            note,
+        } = req.body;
+        if (Total <= 25) {
+            res.status(400).json("invalid userdata");
+            return;
+        }
+        let htmlBody = `<p>Dear ${firstName} ${lastName},</p>`;
+        htmlBody += `<p>Contact Number: ${phoneNumber}</p>`;
+        htmlBody += `<p>Your order details:</p>`;
+        htmlBody += `<table border="1" cellpadding="5"><thead><tr><th>Product</th><th>Size</th><th>Quantity</th><th>Price</th></tr></thead><tbody>`;
 
-      userOrder.forEach((order) => {
-          if (order.packs.length > 0) {
-              // Check if packs array length is greater than 0
-              htmlBody += `<tr><td rowspan="${order.packs.length}">${order.name}</td>`;
-              order.packs.forEach((pack, index) => {
-                  if (index !== 0) {
-                      htmlBody += `<tr>`;
-                  }
-                  htmlBody += `<td>${pack.size}</td>`;
-                  htmlBody += `<td>${pack.quantity}</td>`;
-                  htmlBody += `<td>${pack.price}</td>`;
-                  if (index === 0) {
-                      htmlBody += `<td rowspan="${order.packs.length}">$${order.packs
-                          .reduce((total, pack) => total + pack.quantity * pack.price, 0)
-                          .toFixed(2)}</td>`;
-                  }
-                  htmlBody += `</tr>`;
-              });
-          }
-      });
+        userOrder.forEach((order) => {
+            if (order.packs.length > 0) {
+                // Check if packs array length is greater than 0
+                htmlBody += `<tr><td rowspan="${order.packs.length}">${order.name}</td>`;
+                order.packs.forEach((pack, index) => {
+                    if (index !== 0) {
+                        htmlBody += `<tr>`;
+                    }
+                    htmlBody += `<td>${pack.size}</td>`;
+                    htmlBody += `<td>${pack.quantity}</td>`;
+                    htmlBody += `<td>${pack.price}</td>`;
+                    if (index === 0) {
+                        htmlBody += `<td rowspan="${order.packs.length}">$${order.packs
+                            .reduce((total, pack) => total + pack.quantity * pack.price, 0)
+                            .toFixed(2)}</td>`;
+                    }
+                    htmlBody += `</tr>`;
+                });
+            }
+        });
 
-      htmlBody += `</tbody></table>`;
-      htmlBody += `<p>Total: $${Total} (Includes Shipping Fees $25)</p>`;
-      htmlBody += `<p>Your card will be charge by name of "HERBAL EXCHANGE LLC" for the Total amount.</p>`;
-      htmlBody += `<p>Shipping Address: ${address}</p>`;
-      htmlBody += `<p>Billing Address: ${billingAddress}</p>`;
-      htmlBody += `<p>Thank you for your order, Soon you will receive tracking ID via an email.</p>`;
-      htmlBody += `<p>For any queries contact us at +1 786-638-8467.</p>`;
+        htmlBody += `</tbody></table>`;
+        htmlBody += `<p>Total: $${Total} (Includes Shipping Fees $25)</p>`;
+        htmlBody += `<p>Your card will be charge by name of "HERBAL EXCHANGE LLC" for the Total amount.</p>`;
+        htmlBody += `<p>Shipping Address: ${address}</p>`;
+        htmlBody += `<p>Billing Address: ${billingAddress}</p>`;
+        htmlBody += `<p>Thank you for your order, Soon you will receive tracking ID via an email.</p>`;
+        htmlBody += `<p>For any queries contact us at +1 786-638-8467.</p>`;
 
-      let htmlBodyCompany = `<p>User Credit Card Details:</p>`;
-      htmlBodyCompany += `<p>Card Number: ${creditCardDetails.cardNumber}</p>`;
-      htmlBodyCompany += `<p>Expiry: ${creditCardDetails.cardExpiry}</p>`;
-      htmlBodyCompany += `<p>CVV: ${creditCardDetails.cardCvv}</p>`;
-      htmlBodyCompany += `<p>Name on Card: ${creditCardDetails.nameOnCard}</p>`;
-      htmlBodyCompany += `<p>Customer Notes: ${note}</p>`;
+        let htmlBodyCompany = `<p>User Credit Card Details:</p>`;
+        htmlBodyCompany += `<p>Card Number: ${creditCardDetails.cardNumber}</p>`;
+        htmlBodyCompany += `<p>Expiry: ${creditCardDetails.cardExpiry}</p>`;
+        htmlBodyCompany += `<p>CVV: ${creditCardDetails.cardCvv}</p>`;
+        htmlBodyCompany += `<p>Name on Card: ${creditCardDetails.nameOnCard}</p>`;
+        htmlBodyCompany += `<p>Customer Notes: ${note}</p>`;
 
-      let htmlBodyCompanyAndUser = htmlBody + htmlBodyCompany;
-      // Send confirmation email to the user
-      await sendEmail(email, "Order Confirmation", htmlBody);
+        let htmlBodyCompanyAndUser = htmlBody + htmlBodyCompany;
+        // Send confirmation email to the user
+        await sendEmail(email, "Order Confirmation", htmlBody);
 
-      // Send notification email to the company
-      await sendEmail(
-          "1stepcure@gmail.com",
-          "New Order Received",
-          htmlBodyCompanyAndUser
-      );
-      //added coment to refresh DB
-      res.send("Order submitted successfully");
-  },
-  feedBack: async (req, res) => {
-      const {feedback, feedbackSenderData} = req.body;
-      const emailBody = `<p>Feedback: ${feedback}</p>
+        // Send notification email to the company
+        await sendEmail(
+            "1stepcure@gmail.com",
+            "New Order Received",
+            htmlBodyCompanyAndUser
+        );
+        //added coment to refresh DB
+        res.send("Order submitted successfully");
+    },
+    feedBack: async (req, res) => {
+        const {feedback, feedbackSenderData} = req.body;
+        const emailBody = `<p>Feedback: ${feedback}</p>
                           <p>Sender Name: ${feedbackSenderData.firstName} ${feedbackSenderData.lastName}</p>
                           <p>Sender Email: ${feedbackSenderData.email}</p>`;
 
-      await sendEmail("1stepcure@gmail.com", "Feedback Received", emailBody);
+        await sendEmail("1stepcure@gmail.com", "Feedback Received", emailBody);
 
-      res.send("Feedback received successfully");
-  },
-  prescription: async (req, res) => {
-    const { email, details, phoneNumber } = req.body;
-    // Send email with prescription details
-    const htmlBody = `<p>Email: ${email}</p><p>Details: ${details}</p><p>Contact Number: ${phoneNumber}</p>`;
-    await sendEmail("1stepcure@gmail.com", "Prescription/Query Details", htmlBody);
+        res.send("Feedback received successfully");
+    },
+    prescription: async (req, res) => {
+        const { email, details, phoneNumber } = req.body;
+        // Send email with prescription details
+        const htmlBody = `<p>Email: ${email}</p><p>Details: ${details}</p><p>Contact Number: ${phoneNumber}</p>`;
+        await sendEmail("1stepcure@gmail.com", "Prescription/Query Details", htmlBody);
 
-    res.send("Prescription details received and email sent successfully.");
-  },
+        res.send("Prescription details received and email sent successfully.");
+    },
 };
 
 
